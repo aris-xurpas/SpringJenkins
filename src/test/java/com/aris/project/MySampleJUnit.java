b5dbbabdd7a18fc258f77e5a79e731ea506b3771
aris-xurpas <aris.reguyal@xurpas.com>
2022-02-24 11:46:51 +0800

    Failing Sample
    only to fail the test

---
+++ src/test/java/com/aris/project/MySampleJUnit.java
@@ -0,0 +1,14 @@
+package com.aris.project;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MySampleJUnit {
+
+	@Test
+	void test() {
+		fail("This is a sample test");
+	}
+
+}
